12938ee021c247f06b3dfcf38c8eaaebb580601a
mgree <54516219+soconzan@users.noreply.github.com>
2024-11-14 17:51:16 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,19 +1,18 @@
+with front as (
+    select sum(code)
+    from skillcodes
+    where category = 'Front End'
+)
+
 select case when skill_code & (select code
                                from skillcodes
                                where name = 'Python')
-            and skill_code & (select sum(code)
-                             from skillcodes
-                             where category = 'Front End') then 'A'
+            and skill_code & (select * from front) then 'A'
             when skill_code & (select code
                                from skillcodes
                                where name = 'C#') then 'B'
-            else 'C'
+            when skill_code & (select * from front) then 'C'
        end as grade, id, email
 from developers
-where skill_code & (select sum(code)
-                   from skillcodes
-                   where category = 'Front End')
-or skill_code & (select code
-                from skillcodes
-                where name = 'C#')
+having grade is not null
 order by grade, id;
