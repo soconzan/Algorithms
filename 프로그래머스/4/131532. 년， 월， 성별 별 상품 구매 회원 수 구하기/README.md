bb4621b6a2e758bd4eefa14b4eb486c61ac4d9ff
mgree <54516219+soconzan@users.noreply.github.com>
2024-11-15 10:49:28 +0900

    [level 4] Title: 년, 월, 성별 별 상품 구매 회원 수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/README.md
+++ 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/README.md
@@ -1,6 +1,6 @@
 # [level 4] 년, 월, 성별 별 상품 구매 회원 수 구하기 - 131532 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131532?language=oracle) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131532?language=mysql) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 11월 15일 10:46:53
+2024년 11월 15일 10:49:26
 
 ### 문제 설명
 
--- 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
+++ 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
@@ -1,10 +1,8 @@
-select extract(year from s.sales_date) as year,
-       extract(month from s.sales_date) as month,
+select year(s.sales_date) as year,
+       month(s.sales_date) as month,
        u.gender as gender, count(distinct u.user_id) as users
 from online_sale s
 join user_info u on s.user_id = u.user_id
 where u.gender is not null
-group by extract(year from s.sales_date),
-         extract(month from s.sales_date),
-         u.gender
+group by 1, 2, 3
 order by 1, 2, 3;
