4dead474c5e6b56e6bdfdd0c6b619c67e7299c50
mgree <54516219+soconzan@users.noreply.github.com>
2024-11-15 09:51:30 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -0,0 +1,6 @@
+select id, email, first_name, last_name
+from developers
+where skill_code & (select sum(code)
+                   from skillcodes
+                   where category = 'Front End')
+order by id;
