efceab6140a01ab56864215f73ad64b63e829035
ex.proprietor <54516219+soconzan@users.noreply.github.com>
2025-02-05 13:09:15 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,12 +1,6 @@
-with flavor_rank as (
-    select flavor, sum(total_order), rank() over (order by sum(total_order) desc) as rnk
-    from (select * from first_half
-          union
-          select * from july) as total
-    group by flavor
-)
-
 select flavor
-from flavor_rank
-where rnk <= 3
-order by rnk;
+from (select * from first_half
+     union
+     select * from july) as sales
+group by flavor
+order by sum(total_order) desc limit 3;
