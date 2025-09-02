fb933254d2c48d6567cdab27969663112bee71cb
exwnrl <54516219+soconzan@users.noreply.github.com>
2025-09-02 23:21:04 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(long long n) {
+    vector<int> answer;
+    
+    while (n > 0)
+    {
+        answer.emplace_back(n % 10) ;
+        n /= 10 ;
+    }
+        
+    return answer;
+}
