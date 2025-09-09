94454c95e9ad54bf04e06d7f6f9d91f25deca9ab
exwnrl <54516219+soconzan@users.noreply.github.com>
2025-09-09 13:27:40 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.cc
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -4,17 +4,13 @@ using namespace std ;
 
 int main()
 {
-	int result = 0 ;
 	int num1, num2 ;
 	cin >> num1 >> num2 ;
 
-	for (int i = num2, digit = 1 ; i > 0 ; i /= 10, digit *= 10)
-	{
-		result += num1 * (i % 10) * digit ;
+	for (int i = num2 ; i > 0 ; i /= 10)
 		cout << num1 * (i % 10) << endl ;
-	}
 
-	cout << result ;
+	cout << num1 * num2 ;
 
 	return 0 ;
 }
