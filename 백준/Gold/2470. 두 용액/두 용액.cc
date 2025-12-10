32437f379265328f321224a227e0bb97b3632297
exwnrl <54516219+soconzan@users.noreply.github.com>
2025-12-10 18:49:35 +0900

    [Gold V] Title: 두 용액, Time: 64 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+
+using namespace std ;
+
+int main()
+{
+    int N ;
+    cin >> N ;
+    
+    int arr[N] ;
+    for (int i = 0 ; i < N ; ++i)
+        cin >> arr[i] ;
+    sort(arr, arr + N) ;
+
+    int left = 0, right = N - 1 ;
+    int min = arr[left], max = arr[right] ;
+
+    while ( left < right )
+    {
+        int tmp = arr[left] + arr[right] ;
+
+        if ( abs(tmp) < abs(min + max) )
+        {
+            min = arr[left] ;
+            max = arr[right] ;
+        }
+
+        if ( tmp < 0 )
+            left++ ;
+        else
+            right-- ;
+    }
+    
+    cout << min << " " << max ;
+
+    return 0 ;
+}
